290baceed629981903cd0f2c631ec3ca0ee59293
Burhanuddin <burhanmithai33@gmail.com>
2023-12-15 10:31:08 +0530

    yes

--- app/Http/Controllers/TransactionController.php
+++ app/Http/Controllers/TransactionController.php
@@ -7,10 +7,25 @@ use App\Models\Transaction;
 
 class TransactionController extends Controller
 {
-    public function showtransaction()
+//     public function showtransaction()
+// {
+//     $transactions = Transaction::latest()->get();
+//     return view('transactions', compact('transactions'));
+// }
+
+public function showTransaction(Request $request)
 {
-    $transactions = Transaction::latest()->get();
+    $startDate = $request->input('start_date');
+    $endDate = $request->input('end_date');
+
+    // If both start_date and end_date are provided, filter transactions within the date range
+    $transactions = $startDate && $endDate
+        ? Transaction::whereBetween('created_at', [date("$startDate 00:00:00"), date("$endDate 23:59:59")])->latest()->get()
+        : Transaction::latest()->get();
+
     return view('transactions', compact('transactions'));
 }
+
+
 
 }
